f1f8c398452e2a27e34173ee313aba3a6b6c41a7
JuanCamiloRB2004 <juanc.ramirezb@autonoma.edu.co>
2022-09-21 15:17:00 -0500

    rama main

---
+++ ejercicio.py
@@ -0,0 +1,96 @@
+class datos():
+    def __init__(self):
+        self.diccionario0 = {}
+        self.diccionario1 = {}
+        self.lista = {}
+        self.diccionario2 = {}
+        self.lista2= []
+        self.lista3 = []
+        self.diccionario3 = {}
+    
+    '''
+    def llenarDatos(self):
+        contador = 0
+        while contador <= 2:
+            serie = str(input('serie: '))
+            try:
+                numero_temporadas = int(input('number seasons: '))
+            except: 
+                print('number pls ')
+            lenguaje_original = str(input('original lenguaje: '))
+            ###################
+            print('feature seasons')
+            try:
+                numero_de_temporada = int(input('season number: '))
+            except:
+                print('number pls')
+            nombre_temporada =  str(input('season name: '))
+            fecha = str(input('premier date'))
+            actor1 = str(input('ingrese nombre del primer actor: '))
+            actor2 = str(input('ingrese nombre del segudo actor'))
+            actor3 = str(input('ingrese nombre del tercer actor: '))
+            ####################
+            print('episodes')
+            nombre_episodio = str(input('episode name'))
+            try:
+                duracion = int(input('duration'))
+            except:
+                print('number pls')
+            self.diccionario1["serie"] = serie
+            self.diccionario1["number_season"] = numero_temporadas
+            self.diccionario1["original_lenguaje"] = lenguaje_original
+            
+            self.diccionario2["season_number"] = numero_de_temporada
+            self.diccionario2["season_name"] = nombre_temporada
+            self.diccionario2["premier_date"] = fecha
+            self.lista2.append(actor1)
+            self.lista2.append(actor2)
+            self.lista2.append(actor3)
+            self.diccionario2["cast"] = self.lista2
+            
+            self.diccionario3["episode_name"] = nombre_episodio
+            self.diccionario3["time_duration"] = duracion
+            self.lista3.append(self.diccionario3)
+            
+            self.diccionario2["episodes"] = self.lista3
+            self.lista.append(self.diccionario2)
+            
+            self.diccionario1["features_seasons"] = self.lista
+            
+            self.diccionario0["serie"+str(contador)] = self.diccionario1
+            contador+=1
+            
+    def buscarActor(self):
+        actor_a_buscar = str(input('ingrese el nombre de actor a buscar'))
+        resultado = []
+        arr = []
+        contador1 = 0
+        while contador1 <= len(self.diccionario0):
+            arr = self.diccionario0["serie"+str(contador1)]["features_seasons"][0]["cast"]
+            if actor_a_buscar in arr:
+                resultado.append(self.diccionario0["serie"+str(contador1)]["serie"])
+            contador1 += 1
+        return resultado    
+
+    def buscarPorLenguajeOriginal(self):
+        lenguaje = str(input('ingrese el lenguaje para ver todas las series con este'))
+        resultado = []
+        contador1 = 0
+        while contador1 <= len(self.diccionario0):
+            arr = self.diccionario0["serie"+str(contador1)]["original_lenguaje"]
+            if arr == lenguaje:
+                resultado.append(self.diccionario0["serie"+str(contador1)]["serie"])
+            contador1 += 1
+        return resultado
+    
+    def eliminarSeriePorPremierDate(self):
+        fecha = str(input('ingrese fecha de estreno'))
+        contador1 = 0
+        while contador1 <= 2:
+            arr = self.diccionario0["serie"+str(contador1)]["features_seasons"][0]["premier_date"]
+            if arr == fecha:
+                self.diccionario0["serie"+str(contador1)] = "eliminado"
+            contador += 1
+    '''    
+            
+            
---
+++ main.py
@@ -0,0 +1,7 @@
+'''
+    author: juan camilo ramirez buritica
+    date: 21/09/2022
+    verssion: 1.0
+'''
+from ejercicio import datos
+datos = datos()
